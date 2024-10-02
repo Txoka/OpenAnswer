c86bd28e944da7bfdd0065a4a2d3e339e8ebb896
Txoka <asiertxoka.2@gmail.com>
2024-10-02 12:46:22 +0200

    Tailwind fix

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,8 +1,11 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  content: [
+    './src/**/*.{html,js,jsx,ts,tsx}', // Adjust these paths based on your project structure
+    './public/index.html',
+  ],
   theme: {
     extend: {},
   },
   plugins: [],
-}
+};
