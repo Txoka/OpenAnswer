9763d49756246cdc6f2ab6cb370ac2de96defe4b
Txoka <asiertxoka.2@gmail.com>
2024-10-15 03:58:59 +0200

    Better lua logic for expiration

--- backend/lua_scripts/rate_limiter.lua
+++ backend/lua_scripts/rate_limiter.lua
@@ -7,10 +7,6 @@ local per_ip_limit = {{PER_IP_LIMIT}}
 local total_limit = {{TOTAL_LIMIT}}
 local limit_interval = {{LIMIT_INTERVAL}}
 
--- Initialize requests
-redis.call("SET", ip_key, 0, "NX", "EX", limit_interval)
-redis.call("SET", total_key, 0, "NX", "EX", limit_interval)
-
 -- Check per-IP limit
 local current_ip = tonumber(redis.call("GET", ip_key))
 if current_ip >= per_ip_limit then
@@ -25,9 +21,9 @@ if current_total >= total_limit then
     return {0, "global", ttl} -- Return deny message
 end
 
--- Increment counts
-redis.call("INCR", total_key)
-redis.call("INCR", ip_key)
+-- Increment counts and set expiration
+if redis.call("INCR", total_key) == 1 then redis.call("EXPIRE", total_key, ttl) end
+if redis.call("INCR", ip_key) == 1 then redis.call("EXPIRE", ip_key, ttl) end
 
 -- Return allow message
 return {1, nil, nil}
